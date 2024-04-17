f39b29d8be31e5e4ab6d65a77b57311c5ebeb4af
Sunamun <37673813+mecoxeco@users.noreply.github.com>
2024-04-17 14:45:28 -0300

    update

---
+++ App/test/test.test.js
@@ -0,0 +1,9 @@
+test('adds 1 + 2 to equal 3', () => {
+    expect(1 + 2).toBe(3);
+  });
+
+describe('adds 1 + 2 to equal 3', ()=>{
+    it('get 1 + 2 and compare with value 3', ()=>{
+        expect(1+2).toBe(3);
+    })
+})
