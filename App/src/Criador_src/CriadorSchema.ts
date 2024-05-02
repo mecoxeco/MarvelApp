4ef987712790f2e34dfa22263ae79d94daad385d
Pedro Henrique de Abreu Lourenço <118847988+PedroHLourenco@users.noreply.github.com>
2024-05-02 20:00:24 -0300

    criadorSchema criada

---
+++ App/src/Criador_src/CriadorSchema.ts
@@ -0,0 +1,17 @@
+import mongoose, { Schema, Document, model } from 'mongoose';
+
+export interface Criador extends Document {
+    nome: string;
+    funcao: string;
+    hqsContribuidas: string;
+}
+
+const CriadorSchema: Schema = new Schema({
+    nome: { type: String, required: true },
+    funcao: { type: String, required: true },
+    hqsContribuidas: { type: String, required: true }
+}, {
+    timestamps: true
+});
+
+export default model("Criador", CriadorSchema);
