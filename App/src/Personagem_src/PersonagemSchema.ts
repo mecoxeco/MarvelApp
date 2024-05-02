daca5022bbbb295e8b0eeb4938372f28eac25697
Pedro Henrique de Abreu Lourenço <118847988+PedroHLourenco@users.noreply.github.com>
2024-05-02 20:00:48 -0300

    .

--- App/src/Personagem_src/PersonagemSchema.ts
+++ App/src/Personagem_src/PersonagemSchema.ts
@@ -3,7 +3,7 @@ import mongoose, { Schema, Document, model } from 'mongoose';
 export interface Personagem extends Document {
     nome: string;
     descricao: string;
-    url: Date;
+    url: string;
 }
 
 const PersonagemSchema: Schema = new Schema({
